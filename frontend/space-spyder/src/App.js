b193cb9258db4ea0e39ab5ca235458c21b742787
Desmond Brown <browndesmond30@yahoo.com>
2020-10-16 07:03:40 +0000

    Kali: Added the various routes to the pages

--- frontend/space-spyder/src/App.js
+++ frontend/space-spyder/src/App.js
@@ -1,12 +1,37 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, {useReducer, useState} from 'react';
+import {Route, Switch} from 'react-router-dom';
 
+import './assets/css/spacyder.min.css'
+
+import './assets/vendor/simplebar/dist/simplebar.min.css'
+import './assets/vendor/tiny-slider/dist/tiny-slider.css'
+import './assets/vendor/lightgallery.js/dist/css/lightgallery.min.css'
+import './assets/vendor/flatpickr/dist/flatpickr.min.css'
+
+import Home from "./pages/Home";
+import ComingSoon from "./pages/ComingSoon/ComingSoon";
+import {appReducer, initialAppState} from "./reducers/appReducer";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/404";
+
+
+export const AppContext = React.createContext();
 
 const App = () => {
-    const [cart, setCart] = useState(0);
+    const [state, dispatchState] = useReducer(appReducer, initialAppState);
   return (
-      <div className={"App"}>
-      </div>
+      <AppContext.Provider value={{state: state, dispatchState: dispatchState}}>
+          <div className={"App"}>
+              <Switch>
+                  <Route exact={true} path={'/'} render={()=> <Home />} />
+                  <Route exact={true} path={'/coming-soon'} render={()=> <ComingSoon />} />
+                  <Route exact={true} path={'/about'} render={()=> <About />} />
+                  <Route exact={true} path={'/contact'} render={()=> <Contact />} />
+                  <Route component={NotFound} />
+              </Switch>
+          </div>
+      </AppContext.Provider>
   )
 }
 
